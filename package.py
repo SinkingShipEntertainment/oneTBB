a64487aa12f7a7e46bd4ce1a9b636f84bfc75ed0
Marcelo Sercheli <msercheli@gmail.com>
2021-05-11 18:10:07 -0400

    Add REZ files

---
+++ package.py
@@ -0,0 +1,54 @@
+name = "tbb"
+
+authors = ["Intel"]
+
+version = "2017.6"
+
+description = \
+    """
+    Threading Building Blocks
+    """
+
+with scope("config") as c:
+    # Determine location to release: internal (int) vs external (ext)
+
+    # NOTE: Modify this variable to reflect the current package situation
+    release_as = "ext"
+
+    # The `c` variable here is actually rezconfig.py
+    # `release_packages_path` is a variable defined inside rezconfig.py
+
+    import os
+    if release_as == "int":
+        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_INT"]
+    elif release_as == "ext":
+        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
+
+requires = [
+]
+
+private_build_requires = [
+    "gcc",
+    "swig",
+]
+
+variants = [
+    ["platform-linux", "arch-x86_64", "os-centos-7"]
+]
+
+tools = [
+]
+
+build_command = "bash {root}/rez_build.sh {root}"
+
+def commands():
+    env.TBB_ROOT = "{root}"
+    env.TBB_LOCATION = "{root}"
+
+    env.TBBROOT = "{root}"
+    env.TBB_INSTALL_DIR = "{root}"
+    env.TBB_INCLUDE_DIR = "{root}/include"
+
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+
+uuid = "repository.oneTBB"
